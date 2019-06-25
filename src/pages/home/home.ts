a32ac58deb1d097fada4e9df468572d881eeb680
Hernan <hernag_09@hotmail.com>
2019-06-24 23:23:57 -0300

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { NavController, LoadingController } from 'ionic-angular';
+import { CachedResourceLoader } from '@angular/platform-browser-dynamic/src/resource_loader/resource_loader_cache';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+ mostrar:boolean
+ loader;
+  constructor(public navCtrl: NavController,public loadingCtrl: LoadingController) {
 
   }
 
+  
+
+  aparecer(){
+    this.mostrar=!this.mostrar
+  }
+
+  
 }
